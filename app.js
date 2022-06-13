b47d2b52e8bec73dae5bdc6e26c76e902cb2a8bf
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-06-13 17:47:20 -0400

    added JavaScript to fetch api on click and page load

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const dice = document.querySelector(".dice-container");
+
+const getAdviceQuote = () => {
+  const adviceNumber = document.querySelector(".advice-number");
+  const adviceQuote = document.querySelector(".advice-quote");
+
+  fetch("https://api.adviceslip.com/advice")
+    .then((resp) => resp.json())
+    .then((data) => {
+      adviceNumber.textContent = `ADVICE #${data.slip.id}`;
+      adviceQuote.textContent = `"${data.slip.advice}"`;
+    });
+};
+
+dice.addEventListener("click", () => {
+  getAdviceQuote();
+});
+
+getAdviceQuote();
